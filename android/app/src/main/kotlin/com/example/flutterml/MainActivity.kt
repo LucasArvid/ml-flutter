e446fdc36d89adaf72d9ee33601877af6361655f
LucasArvid <lucas.arvidsson@hotmail.com>
2020-05-06 18:08:04 +0200

    rewrote for dataset

---
+++ android/app/src/main/kotlin/com/example/flutterml/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterml
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:tflite/tflite.dart';
+import 'dart:developer';
+import "dart:typed_data";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TfliteHome(),
+    );
+  }
+}
+
+class TfliteHome extends StatefulWidget {
+  @override
+  _TfliteHomeState createState() => _TfliteHomeState();
+}
+
+class _TfliteHomeState extends State<TfliteHome> {
+  File _file;
+  int _numOfPredictions = 100;
+  
+  List _recognitions;
+
+  @override
+  void initState() {
+    super.initState();
+    runExperiment();  
+  }
+
+void runExperiment() async {
+  await loadModel();
+  for (int i = 1; i <= _numOfPredictions; i++) {
+      await prepareImage(i);
+      await getImage();
+  } 
+  exit(0);
+}
+
+// Loads float point mobilnet model, expects 224x224 images
+  loadModel() async {
+    Tflite.close();
+    try { 
+      String res;
+      res = await Tflite.loadModel(
+        model: "assets/tflite/mobilenet_v1_1.0_224.tflite",
+        labels: "assets/tflite/mobilenet_v1_1.0_224.txt",
+        numThreads: 1,
+      );
+    } on PlatformException {
+    }
+  }
+
+// Prepares the image by getting path
+  prepareImage(int iteration) async {
+    ByteData data = await rootBundle.load('assets/images/image_00$iteration.jpg');
+    log("image: image_00$iteration.jpg");
+    String directory = (await getTemporaryDirectory()).path;
+    File file = await writeToFile(data, '$directory/image_$iteration.jpg');
+    
+    setState(() {
+      _file = file;
+    });
+  }
+
+// Function called when pressing buttons, starts timer and does set amount of inferences.
+  Future getImage() async {
+    await predictImageClassification();
+    setState(() {
+    });
+
+  }
+// Image inference, does ImageProcess (bitmap to ByteBuffer) and tflite inference.
+  predictImageClassification() async {
+    var recognitions = await Tflite.runModelOnImage(
+      path: _file.path,
+      numResults: 6,
+      threshold: 0.05,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      asynch: false,
+    );
+
+    setState(() {
+      _recognitions = recognitions;
+    });
+  }
+
+Future<File> writeToFile(ByteData data, String path) {
+  ByteBuffer buffer = data.buffer;
+  return File(path).writeAsBytes(buffer.asUint8List(
+    data.offsetInBytes,
+    data.lengthInBytes,
+  ));
+
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
